02b4c02a4ac391c3c27647dcbb206fb3808bdbe7
Red Python <cornesalesve@gmail.com>
2024-12-06 12:09:13 -0400

    Da Vinci Restaurant

--- clientes.py
+++ clientes.py
@@ -3,7 +3,7 @@ class Cliente:
     _next_id = 1
 
     def __init__(self, tamaño_grupo):
-        self.id(f"C{Cliente._next_id:o3d}")
+        self.id(f"C{Cliente._next_id:03d}")
         Cliente.id_cliente += 1
         self.tamaño_grupo = tamaño_grupo
         self.pedido_actual = None
